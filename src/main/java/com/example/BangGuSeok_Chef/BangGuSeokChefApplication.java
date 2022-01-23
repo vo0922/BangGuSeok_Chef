482cd7356f28d72ec1cd25395fc1915d5379404a
vo0922 <vo0922@naver.com>
2022-01-23 19:24:04 +0900

    start

---
+++ src/main/java/com/example/BangGuSeok_Chef/BangGuSeokChefApplication.java
@@ -0,0 +1,13 @@
+package com.example.BangGuSeok_Chef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangGuSeokChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangGuSeokChefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BangGuSeok_Chef/BangGuSeokChefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BangGuSeok_Chef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangGuSeokChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
