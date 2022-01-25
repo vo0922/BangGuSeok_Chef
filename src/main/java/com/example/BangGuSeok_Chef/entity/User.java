f6be753e3e230ffa2a7160a05c80b25e2c9c8664
vo0922 <vo0922@naver.com>
2022-01-25 23:50:12 +0900

    Mysql 연동

---
+++ src/main/java/com/example/BangGuSeok_Chef/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.BangGuSeok_Chef.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private Character gender;
+
+    @Column
+    private Long year;
+
+    public void patch(User user) {
+
+    }
+}
