851d65339018aa1aba4145fc866c146d5bca8319
vo0922 <vo0922@naver.com>
2022-02-17 17:14:23 +0900

    front_end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect, useState} from 'react';
+// routes
+import Router from './routes';
+// theme
+import ThemeConfig from './theme';
+import GlobalStyles from './theme/globalStyles';
+// components
+import ScrollToTop from './components/ScrollToTop';
+import { BaseOptionChartStyle } from './components/charts/BaseOptionChart';
 
-function App() {
-  const [message, setMessage] = useState([]);
+// ----------------------------------------------------------------------
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((res) => {
-        return res.json();
-      })
-      .then(function (data) {
-        setMessage(data);
-      })
-  })
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ul>
-        {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
-        </ul>
-      </header>
-    </div>
+    <ThemeConfig>
+      <ScrollToTop />
+      <GlobalStyles />
+      <BaseOptionChartStyle />
+      <Router />
+    </ThemeConfig>
   );
 }
-
-export default App;
